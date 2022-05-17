802492f5e8b5254f5fbdbc1d681292e794167b14
Gauravkakran17 <gaurav0499.be21@chitkara.edu.in>
2022-05-17 15:48:22 +0530

    Files Added By Gaurav Aggarwal

---
+++ Order Medicines/products.js
@@ -0,0 +1,39 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "LevoCetirizine",
+    desc : "To Treat Allergies,itching nose and sneezing.",
+    img : "levocetirizine.png",
+    price : 235
+  },
+  124: {
+    name : "Paracetamol",
+    desc : "Painkiller used to treat aches and pa in",
+    img : "paracetamol.png",
+    price : 80
+  },
+  125: {
+    name : "Cough Syrup",
+    desc : "To Treat Sore Throat and Cough.",
+    img : "coughsyrup.png",
+    price : 70
+  },
+  126: {
+    name : "Pain Relief Spray",
+    desc : "Spray for instant relief from pain.",
+    img : "painrspray.png",
+    price : 90
+  },
+  127: {
+    name : "Masks (10pc)",
+    desc : "To protect yourself and people around you",
+    img : "masks.png",
+    price : 50
+  },
+  128: {
+    name : "N95 Masks (10pc)",
+    desc : "To protect yourself and people around you",
+    img : "n95.png",
+    price : 150
+  }
+};
